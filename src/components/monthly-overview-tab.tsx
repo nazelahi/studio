0323716d837fcb8c795d110228787a1f65135f9d
It Solution <najalahi9@gmail.com>
2025-07-11 09:15:15 +0000

    remove add rent button from rent roll

--- src/components/monthly-overview-tab.tsx
+++ src/components/monthly-overview-tab.tsx
@@ -534,12 +534,6 @@ export function MonthlyOverviewTab({ year }: { year: number }) {
                             Sync Tenants
                         </Button>
                         <Dialog open={isRentDialogOpen} onOpenChange={handleRentOpenChange}>
-                          <DialogTrigger asChild>
-                             <Button size="sm" className="gap-2">
-                              <PlusCircle className="h-4 w-4" />
-                              Add Rent
-                            </Button>
-                          </DialogTrigger>
                             <DialogContent>
                                 <DialogHeader>
                                     <DialogTitle>{editingRentEntry ? 'Edit Rent Entry' : 'Add New Rent Entry'}</DialogTitle>
@@ -1030,5 +1024,3 @@ export function MonthlyOverviewTab({ year }: { year: number }) {
     </>
   )
 }
-
-    
