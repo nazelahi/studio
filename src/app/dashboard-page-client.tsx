0425d4a17b996bea828c29d7afd7110296f445fb
It Solution <najalahi9@gmail.com>
2025-07-19 08:25:36 +0000

    remove Toggle theme from settings icon dropdown

--- src/app/dashboard-page-client.tsx
+++ src/app/dashboard-page-client.tsx
@@ -316,18 +316,6 @@ export default function DashboardPageClient() {
                                 <Settings className="mr-2 h-4 w-4" />
                                 <span>Settings</span>
                                 </DropdownMenuItem>
-                                <DropdownMenuSub>
-                                <DropdownMenuSubTrigger>
-                                    <Sun className="h-4 w-4 mr-2 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                                    <Moon className="absolute h-4 w-4 mr-2 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                                    <span>Toggle theme</span>
-                                </DropdownMenuSubTrigger>
-                                <DropdownMenuSubContent>
-                                    <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-                                    <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-                                    <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
-                                </DropdownMenuSubContent>
-                                </DropdownMenuSub>
                                 <DropdownMenuSeparator />
                                 <DropdownMenuItem onClick={handleSignOut}>
                                 <LogOut className="mr-2 h-4 w-4" />
